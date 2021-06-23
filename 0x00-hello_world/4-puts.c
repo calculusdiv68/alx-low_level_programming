1b52b348601ebeba824d0e121ad54da36c1dc48e
calculusdiv68 <calculusdiv68@gmail.com>
2021-06-23 16:36:21 -0700

    FIxes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,16 @@
-#!/bin/bash
 #include <stdio.h>
 
-   /**
-    * main - Entry point
-    *
-    *Return: Always 0
-    */
-
+/**
+*
+*
+*main - Entry point
+*
+*
+*Return: Always 0
+*/
 int main(void)
 {
-  puts("Programming is like building a multilingual puzzle"\n);
+    puts("Programming is like building a multilingual puzzle\n");
+    return 0;
 
-    return (0);
 }
