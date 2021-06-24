e586a9cf514521f0695ee17db20c3f675e3b2a4f
calculusdiv68 <calculusdiv68@gmail.com>
2021-06-24 08:10:40 -0700

    Betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,10 +4,10 @@
  *
  *Description - main returns 0
  *Puts - Writes string and newline to stdout
- *return: 0
+ *return: Always 0
  */
 int main(void)
 {
- puts("\"Programming is like building a multilingual puzzle");
- return (0);
+   puts("\"Programming is like building a multilingual puzzle");
+   return (0);
 }
