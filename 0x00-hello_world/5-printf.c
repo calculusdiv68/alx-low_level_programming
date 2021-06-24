dfd5fb73be7ff888c28b552baead9246be226844
calculusdiv68 <calculusdiv68@gmail.com>
2021-06-24 08:43:57 -0700

    printf usage

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - execute the file
+ *
+ *Description - main executes the inputed codes and returns 0
+ *printf - prints string with new line
+ *Return: 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
