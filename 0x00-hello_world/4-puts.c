612f81a54b1b4ecca0bfbf117de000057e5c25a4
calculusdiv68 <calculusdiv68@gmail.com>
2021-06-24 07:44:09 -0700

    Corrected betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
 /**
- *Puts - Used in priting
+ ;l*Main-return 0
  *
+ *Description - main returns 0
+ *Puts - Write and prints out a string to stdout
+ *Return : 0(success)
  */
 
 int main(void)
