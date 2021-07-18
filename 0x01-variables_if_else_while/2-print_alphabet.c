d9c25f2fb9f020e56b52375cac6cd24802d5881f
calculusdiv68 <calculusdiv68@gmail.com>
2021-07-17 21:08:42 -0700

    Printing to lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - returns 0
+ *
+ *Description - main returns 0
+ *putchar -Prints each letter of the alphabet in lowercase
+ *Return: 0
+ */
+int main(void)
+{
+char a = 'a'
+while (a <= 'z')
+{
+putchar(a'\n');
+a++
+return(0);
+}
