1f719aaad23759885d737951cbc0b47955cd2c7d
calculusdiv68 <calculusdiv68@gmail.com>
2021-07-17 20:53:38 -0700

    update

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,7 +10,7 @@
  */
 int main(void)
 {
-int n; 
+int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n % 10 > 5)
@@ -23,7 +23,7 @@ printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 }
 else
 {
-printf("last digit of %d is %d and is 0\n", n, n % 10);
+printf("Last digit of %d is %d and is 0\n", n, n % 10);
 }
 return (0);
 }
