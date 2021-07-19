f22b4b9a1a53b1075b6534bf0d703f29c50ba945
calculusdiv68 <calculusdiv68@gmail.com>
2021-07-18 20:51:01 -0700

    Exceptions

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main - returns 0
+ *
+ *Description - main returns 0
+ * putchar - print the alphabet in lowercase excepte and q
+ *Return: 0
+ */
+int main(void)
+{
+char a;
+for (a = 'a'; a <= 'z'; a++)
+{
+if (a != 'e' && a != 'q')
+{
+putchar(a);
+}
+}
+putchar('\n')
+return (0);
+}
