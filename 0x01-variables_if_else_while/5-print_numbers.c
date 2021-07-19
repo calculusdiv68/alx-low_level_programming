b6a16c43d29af40bb846c280c0d26ce51c2644eb
calculusdiv68 <calculusdiv68@gmail.com>
2021-07-18 21:09:07 -0700

    Base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - returns 0
+ *
+ * Description - main returns 0
+ *printf- Prints each prints all single digit numbers of base 10
+ *Return: 0
+ */
+int main(void)
+{
+int s;
+for(s = 0; s < 10; s++)
+{
+printf("%d", s);
+}
+printf('\n');
+return (0);
+}
