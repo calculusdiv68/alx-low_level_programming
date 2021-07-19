8322904f0db473a1a37ac5d6a7e3c77504cf8b54
calculusdiv68 <calculusdiv68@gmail.com>
2021-07-18 21:38:45 -0700

    numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - returns 0
+ *
+ *Description - Single digits of base 10
+ *Putchar prints 
+ *
+ *Return: 0
+ */
+int main(void)
+{
+int a;
+for (a = 0; a < 10; a++)
+{
+putchar(a)
+}
+putchar('\n');
+return (0);
+}
