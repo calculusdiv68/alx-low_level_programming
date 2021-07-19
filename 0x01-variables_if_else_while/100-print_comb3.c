855b398f01648daba19b5551509a639496837f65
calculusdiv68 <calculusdiv68@gmail.com>
2021-07-18 23:01:28 -0700

    update

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,12 +11,16 @@ for (a =0; a < 10; a++)
 {
 for (b = 0; b < 10; b++)
 {
-putchar((a % 10) + '0');
-putchar((b % 10) + '0');
-if (a == 9 && b == 9)
-continue;
+if (a < b)
+{
+putchar(a);
+putchar(b);
+}
+if (a != 8 || ( a == 8 && b != 9)
+{
 putchar(',');
 putchar(' ');
+}
 }
 }
 putchar('\n');
