e1cd6bffaab7df8122d1b1e4c6e6510645b5c63c
calculusdiv68 <calculusdiv68@gmail.com>
2021-07-18 21:58:01 -0700

    reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - return 0
+ *
+ *Description - Reverse printing of alphabet
+ *
+ *Return: 0
+ */
+int main(void)
+{
+int a;
+for (a = 'z'; a >= 'a', a--)
+{
+putchar(a);
+}
+putchar('\n';)
+return (0);
+}
