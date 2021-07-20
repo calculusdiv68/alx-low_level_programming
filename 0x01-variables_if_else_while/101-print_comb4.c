6904e175aaabceca030ce350d5955b12afe63b62
calculusdiv68 <calculusdiv68@gmail.com>
2021-07-19 20:45:11 -0700

    combo trio

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+/**
+ *
+ *main -returns 0
+ *
+ *Description - combo trio
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int a, b, c;
+a = 48;
+b = 49;
+c = 50;
+while ((a < 56) && (b < 57) &&  (c < 58))
+{
+putchar(a);
+putchar(b);
+putchar(c);
+if ((a == 55) && (b == 56) && (c == 57))
+{
+putchar('\n');
+a++
+b++
+c++
+}
+else
+{
+putchar(',');
+putchar(' ');
+if (c < 57)
+{
+c++;
+}
+else if ((b < 56) && (c == 57))
+{
+c = 1 + ++b;
+}
+else
+{
+b = ++a + 1;
+c = b + 1;
+}
+}
+}
+return (0);
+}
