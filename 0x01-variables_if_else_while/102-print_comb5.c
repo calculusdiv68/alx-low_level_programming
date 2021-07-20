c244a567580e791e5655a5221d787d7fc9e91b7d
calculusdiv68 <calculusdiv68@gmail.com>
2021-07-19 21:07:21 -0700

    final combo

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - return 0
+ * 
+ * Description - main return zero in combo
+ * Return:0
+ */
+int main(void)
+{
+int i, j
+for (i = 0; i <= 98; i++)
+{
+putchar(1 / 10) + '0');
+putchar(i % 10) + '0');
+putchar(' ');
+putchar((j / 10) + '0');
+putchar((j % 10) + '0');
+if ((i == 98) && (j == 99))
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
